30e8f872191faeb4c35907712ebbe198a191a881
Dmitrii Derepko <xepozz@list.ru>
2022-04-16 11:10:27 +0300

    Fix creating Validator

--- tests/Support/TestCase.php
+++ tests/Support/TestCase.php
@@ -15,7 +15,8 @@ use Yiisoft\RequestModel\RequestModelFactory;
 use Yiisoft\Router\CurrentRoute;
 use Yiisoft\Router\Route;
 use Yiisoft\Test\Support\Container\SimpleContainer;
-use Yiisoft\Validator\Rule\Required\RequiredValidator;
+use Yiisoft\Validator\Rule\Required\RequiredHandler;
+use Yiisoft\Validator\Tests\Stub\StaticRuleHandlerResolver;
 use Yiisoft\Validator\Validator;
 
 abstract class TestCase extends BaseTestCase
@@ -27,7 +28,6 @@ abstract class TestCase extends BaseTestCase
                 SimpleMiddleware::class => new SimpleMiddleware(),
                 SimpleController::class => new SimpleController(),
                 CurrentRoute::class => $this->getCurrentRoute(),
-                RequiredValidator::class => new RequiredValidator(),
             ]
         );
     }
@@ -49,7 +49,10 @@ abstract class TestCase extends BaseTestCase
 
     public function createRequestModelFactory(ContainerInterface $container): RequestModelFactory
     {
-        return new RequestModelFactory(new Validator($container), new Injector($container), $this->getCurrentRoute());
+        $validator = new Validator(new StaticRuleHandlerResolver([
+            RequiredHandler::class => new RequiredHandler(),
+        ]));
+        return new RequestModelFactory($validator, new Injector($container), $this->getCurrentRoute());
     }
 
     private function getCurrentRoute(): CurrentRoute
